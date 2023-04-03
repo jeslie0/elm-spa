cc3a5921367df5fd2c29397c7c51b559a5b784f2
James Leslie <jamesleslie@posteo.net>
2023-04-03 20:46:53 +0100

    Add elmSpaGen derivation

--- flake.nix
+++ flake.nix
@@ -19,15 +19,26 @@
           };
       in
         {
-          packages.default = pkgs.buildNpmPackage {
-            pname = "elm-spa";
-            version = "6.0.4";
-            src = "${elm-spa-repo}/src/cli";
-            npmDepsHash = "sha256-yHcbrNkrYc+8iQ5ZZj5SJIYQxmDwq5yoYg67yfuQJBQ=";
-            postInstall = ''
+          packages = {
+            default = pkgs.buildNpmPackage {
+              pname = "elm-spa";
+              version = elm-spa-version;
+              src = "${elm-spa-repo}/src/cli";
+              npmDepsHash = "sha256-yHcbrNkrYc+8iQ5ZZj5SJIYQxmDwq5yoYg67yfuQJBQ=";
+              postInstall = ''
                         wrapProgram $out/bin/elm-spa \
                         --prefix PATH : ${pkgs.elmPackages.elm}/bin
                         '';
+            };
+            elmSpa = self.packages.${system}.default;
+            elmSpaGen = pkgs.stdenvNoCC.mkDerivation {
+              name = "elm-spa-gen";
+              src = ./.;
+              buildDependencies = [ self.packages.${system}.default ];
+              buildPhase = "${self.packages.${system}.default}/bin/elm-spa gen";
+              installPhase = "mkdir $out; cp -r .elm-spa $out";
+              meta.description = "Create the .elm-spa directory for elm-spa to use.";
+            };
           };
       }
     );
